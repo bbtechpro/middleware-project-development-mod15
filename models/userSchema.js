e813d6a363a3223466e73ff2876806da77e19670
bbtechpro <billy.barber@gmail.com>
2026-05-26 09:46:10 -0400

    Define a userSchema that includes fields for username, email (must be unique), and password. It must include a pre-save hook to hash the password using bcrypt,

---
+++ models/userSchema.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt');
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+ 
+const User = model('User', userSchema);
+
+module.exports = User;
